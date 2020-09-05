e5be9d2ebba943380f6685f7616ed5f0e5a07f61
jiho1996 <jiho.urssu@gmail.com>
2020-09-05 23:24:42 +0900

    수정

--- app/src/main/java/kr/com/ticketpass/login/HostLoginActivity.kt
+++ app/src/main/java/kr/com/ticketpass/login/HostLoginActivity.kt
@@ -34,13 +34,13 @@ class HostLoginActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login_host)
         binding.viewModel = viewModel
     }
-  /*  fun changeFragment(f: HostSignUpEmailFragment, cleanStack: Boolean = false) {
+  /* fun changeFragment(f: HostSignUpEmailFragment, cleanStack: Boolean = false) {
         val ft = supportFragmentManager.beginTransaction() // (2) 프레그먼트 관리자를 통한 제어
-        ft.replace(R.id.host_login_signup_text_host, f) // (3) 프레그먼트의 변경
+        ft.add(R.id.host_login_signup_text_host, f) // (3) 프레그먼트의 변경
         ft.addToBackStack(null) // (4) 백스택에 넣기
         ft.commit() // (5) 최종 프레그먼트의 적용
     }
-    */
+*/
 
 
 }
