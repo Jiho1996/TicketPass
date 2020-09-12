a09fe3b58e8a42fd63373b95b7855d995d514e41
Teddy_changik <aackddlr@gmail.com>
2020-09-12 22:25:37 +0900

    추가

--- app/src/main/java/kr/com/ticketpass/host/HostManageActivity.kt
+++ app/src/main/java/kr/com/ticketpass/host/HostManageActivity.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_host_about_event.*
 import kr.com.ticketpass.R
+import kr.com.ticketpass.model.TicketResponse
 
 class HostManageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_host_about_event)
+
+        val ticket = intent.getSerializableExtra("ticket") as TicketResponse.TicketInfo
 
         event_info_button.setOnClickListener {
             startActivity(Intent(this, HostEventManageActivity::class.java))
